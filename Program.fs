39484a52a6dd5a5eaac2ee63b336dc93ac2e657f
ParaDev-RP <Haniff.Murray@Paradyme.us>
2023-02-25 15:04:09 -0500

    changes

--- Program.fs
+++ Program.fs
@@ -4,31 +4,44 @@ open System.Linq
 // Define a function to construct a message to print
 let from whom = sprintf "from %s" whom
 
-let floatAdd a b =
+let optionAdd a b =
     match a, b with
     | Some x, Some y -> Some(x + y)
     | Some x, _ -> Some(x)
     | _, Some y -> Some(y)
     | _ -> None
 
+let optionDiv a b =
+    match a with
+    | Some x -> Some(x / float b)
+    | _ -> None
+
 type Observation =
     { test_level: int
       test_value: float option }
 
-type TreeNode = { N: int; Reward: float option }
+type TreeNode = { N: float; Reward: float option }
 
 type Tree =
     | Node of data: Observation
     | Branch of data: TreeNode * left: Tree * right: Tree
-    member this.N() =
+    member this.N =
         match this with
-        | Node x -> 1
-        | Branch (d, l, r) -> l.N() + r.N()
+        | Node x -> 1.0
+        | Branch (d, l, r) -> d.N
 
     member this.Reward() =
         match this with
         | Node x -> x.test_value
-        | Branch (d, l, r) -> floatAdd (l.Reward()) (r.Reward())
+        | Branch (d, l, r) -> d.Reward
+
+    member this.AvgReward =
+        match this with
+        | Node x -> x.test_value
+        | Branch (d, l, r) ->
+            let reward_sum = optionAdd (l.Reward()) (r.Reward())
+            let cnt = l.N + r.N
+            optionDiv reward_sum cnt
 
 let real_data: Observation list =
     [ { test_level = 1
@@ -37,6 +50,8 @@ let real_data: Observation list =
         test_value = Some 0.0 }
       { test_level = 4
         test_value = Some 1.0 }
+      { test_level = 4
+        test_value = Some 0.0 }
       { test_level = 5
         test_value = Some 0.0 } ]
 
@@ -60,16 +75,59 @@ let rec TreeBuilder data_seq =
             let right_tree: Tree = TreeBuilder right
 
             let left_node =
-                { N = left_tree.N() + right_tree.N()
-                  Reward = floatAdd (left_tree.Reward()) (right_tree.Reward()) }
+                { N = left_tree.N + right_tree.N
+                  Reward = optionAdd (left_tree.Reward()) (right_tree.Reward()) }
 
             Branch(left_node, TreeBuilder left, TreeBuilder right)
 
     retval
 
+let rec PickNode data_tree =
+    let logFunc (t: Tree) total_N =
+        let log_stuff = 2.0 * log t.N
+        let sqrt_stuff = sqrt log_stuff / total_N
+        Some(sqrt_stuff)
+
+    let NodeEval (l: Tree) (r: Tree) =
+        if l.Reward() = None then
+            PickNode l
+        else if r.Reward() = None then
+            PickNode r
+        else
+            let total_N = float (l.N + r.N)
+            let l_score = optionAdd (logFunc l total_N) l.AvgReward
+            let r_score = optionAdd (logFunc r total_N) r.AvgReward
+
+            if l_score > r_score then
+                PickNode l
+            else
+                PickNode r
+
+    // evaluate missing data, then UCB
+    match data_tree with
+    | Node (x) -> x
+    | Branch (d, l, r) -> NodeEval l r
+
+let ObsCompact (observations: Observation list) =
+    observations
+    |> List.groupBy (fun o -> o.test_level)
+    |> List.map (fun (testLevel, obsList) ->
+        let values =
+            obsList
+            |> List.choose (fun o -> o.test_value)
+            |> List.map float
+
+        let average = values |> List.averageBy id |> Some
+
+        { test_level = testLevel
+          test_value = average })
+
+
 [<EntryPoint>]
 let main argv =
     let data_list = List.init 25 (fun i -> { test_level = i; test_value = None })
+
+    let clean_data = ObsCompact real_data
 
     let query1 =
         query {
@@ -95,6 +153,9 @@ let main argv =
 
     let data_tree = TreeBuilder data_sequence
 
-    printfn "%A" data_tree
+    let NextExperiment = PickNode data_tree
+
+    printfn "data: %A" data_tree
+    printfn "next: %A" NextExperiment
 
     0 // return an integer exit code
